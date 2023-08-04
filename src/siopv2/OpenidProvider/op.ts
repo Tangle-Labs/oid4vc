b8a396d2d1d3478d5ef4b49e06d8ac8ef567f76c
Merul Dhiman <69296233+coodos@users.noreply.github.com>
2023-08-04 19:22:02 +0530

    fix: missing state (#19)
    * fix: missing state
    
    * chore: bump version

--- src/siopv2/OpenidProvider/op.ts
+++ src/siopv2/OpenidProvider/op.ts
@@ -139,7 +139,11 @@ export class OpenidProvider {
                 requestOptions
             );
         }
-        response = { ...response, nonce: requestOptions.nonce };
+        response = {
+            ...response,
+            nonce: requestOptions.nonce,
+            state: requestOptions.state,
+        };
         await axios.post(requestOptions.redirectUri, response).catch(() => {
             throw new Error("unable to send response");
         });

c4b16fcdca52bc4404a476fa6c2f56b4797805c7
Coodos <info@coodos.co>
2023-08-04 07:32:41 +0530

    chore: init jest

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+
+const config: Config = {
+    collectCoverage: true,
+
+    coverageDirectory: "coverage",
+
+    coverageProvider: "v8",
+};
+
+export default config;
